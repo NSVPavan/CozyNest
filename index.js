859755382239a20db5df69538c8556905b0c88fa
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-04 17:18:54 +0530

    Create and test listing model in the database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
 
 const app = express();
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozynest";
@@ -20,4 +21,17 @@ app.listen(8080,()=>{
 
 app.get('/',(req,res)=>{
     res.send('Hi! this the home page');
+})
+
+app.get('/testListing',async (req,res)=>{
+    let sampleListing = new Listing({
+        title: "Heaven",
+        description: "Literally the heaven",
+        price : 1500,
+        location: "Swarg",
+        country: "India"
+    });
+    await sampleListing.save();
+    console.log("Sample listing saved");
+    res.send("Done creating test listing");
 })
