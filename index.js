2e4fd897e5be767e2f96465ce157598dd79dfdba
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-06 02:09:34 +0530

    Implement /listings route to show all listings

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
+const path = require('path');
 
 const app = express();
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozynest";
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
 main().
 then(()=>{
@@ -17,21 +21,13 @@ async function main() {
 
 app.listen(8080,()=>{
     console.log('listening on port 8080');
-})
+});
 
 app.get('/',(req,res)=>{
     res.send('Hi! this the home page');
-})
+});
 
-app.get('/testListing',async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "Heaven",
-        description: "Literally the heaven",
-        price : 1500,
-        location: "Swarg",
-        country: "India"
-    });
-    await sampleListing.save();
-    console.log("Sample listing saved");
-    res.send("Done creating test listing");
-})
+app.get('/listings',async (req,res)=>{
+    let all_listings = await Listing.find({});
+    res.render("listings/index.ejs",{all_listings});
+});
