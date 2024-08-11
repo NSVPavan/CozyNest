932b973f239f91a3d909d99d687ccec89cd226ce
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-11 22:27:58 +0530

    Add navbar to layout

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/cozynest";
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"/public")));
 app.use(methodeOverride("_method"));
 app.engine("ejs",ejsMate);
 
