d9e886009d2a385e747743c44221aca3a9760e10
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-19 00:56:03 +0530

    Add error.ejs to display handled errors to users

--- index.js
+++ index.js
@@ -102,5 +102,5 @@ app.get("*",(req,res,next)=>{
 //error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode="500",message="Something went wrong!"} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("listings/error.ejs",{message});
 })
