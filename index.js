e18736fa71556366defb160d7fec23466c63d74c
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-29 02:19:58 +0530

    chore: Configure passport

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 // Models and Utilities
 const Listing = require('./models/listing.js');
 const Review = require('./models/review.js');
 const wrapAsync = require('./utils/wrapAsync.js');
+const User = require('./models/user.js');
 const ExpressError = require('./utils/ExpressError.js');
 
 // Schema validation
@@ -69,6 +72,13 @@ app.get('/', (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
