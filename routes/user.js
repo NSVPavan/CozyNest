b6cbeceadb8d61cb7d457a984d97bca163b848ef
NSVPavan <sivavenkatapavan@gmail.com>
2024-08-30 22:45:17 +0530

    feat: Register user to DB after signup
    - Flash success message on valid signup
    - Flash error message on invalid signup

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const router = express.Router({mergeParams: true});
-const User = require('../models/user.js');
-const wrapAsync =  require('../utils/wrapAsync.js');
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs")
-})
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
 
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { email, username, password } = req.body;
+      const newUser = new User({ email, username });
+      let registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to cozynest!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
 module.exports = router;
