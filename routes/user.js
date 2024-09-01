95aa1d5bdf326427557828d9e4a5b89887c25a58
NSVPavan <sivavenkatapavan@gmail.com>
2024-09-01 17:07:15 +0530

    feat: Redirect user to original page after login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -34,14 +35,16 @@ router.get("/login", (req, res) => {
 });
 
 router.post(
-  "/login",
+  "/login",saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   wrapAsync(async (req, res) => {
    req.flash("success",`Welcome back ${req.body.username}`);
-   res.redirect("/listings");
+   let redirectUrl = (res.locals.redirectUrl) || "/listings";
+   console.log(redirectUrl);
+   res.redirect(redirectUrl);
   })
 );
 
